832df2078f027c3b69055179adf1ca64fd61b008
jiagehao <836169891@qq.com>
2018-06-01 16:22:06 +0800

    diyi

---
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/ItemServiceImpl.java
@@ -0,0 +1,30 @@
+package cn.e3mall.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.e3mall.mapper.TbItemMapper;
+import cn.e3mall.pojo.TbItem;
+
+/**
+ * 商品管理Service
+ * <p>Title: ItemServiceImpl</p>
+ * <p>Description: </p>
+ * <p>Company: www.itcast.cn</p> 
+ * @version 1.0
+ */
+@Service
+public class ItemServiceImpl implements ItemService {
+
+	
+
+	@Autowired
+	private TbItemMapper itemMapper;
+	
+	@Override
+	public TbItem getItemById(long id) {
+		TbItem item = itemMapper.selectByPrimaryKey(id);
+		return item;
+	}
+
+}
